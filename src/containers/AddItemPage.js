710a959e562d2225392540646c015aa29b88fe79
Pavlo <p.tern2298@gmail.com>
2022-12-06 15:37:32 -0500

    - Components Add info commit

--- src/containers/AddItemPage.js
+++ src/containers/AddItemPage.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import MainPageHeader from '../components/MainPageHeader';
 
 function AddItemPage() {
     return(
         <div>
-            <div>Page header</div>
             <div>
-                <p>Add item page</p>
+                <MainPageHeader/>
+            </div>
+            <div>
+                <p>Add new framework</p>
+            </div>
+            <div>
+                <form>
+                    <p>Fields</p>
+                </form>
+            </div>
+            <div>
+                <p>Buttons</p>
             </div>
         </div>
     )
--- src/containers/EditItemPage.js
+++ src/containers/EditItemPage.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import MainPageHeader from '../components/MainPageHeader';
 
 function EditItemPage() {
     return (
         <div>
-            <div>Page header</div>
             <div>
-                <p>Edit item page</p>
+                <MainPageHeader/>
+            </div>
+            <div>
+                <p>Edit framework</p>
+            </div>
+            <div>
+                <form>
+                    <p>Fields</p>
+                </form>
+            </div>
+            <div>
+                <p>Buttons</p>
             </div>
         </div>
     )
